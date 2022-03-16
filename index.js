2987564e441c1b31b82e4d80c569a88f659912e4
Supanat Wanroj <supanat.wanroj@scbtechx.io>
2022-03-16 17:58:09 +0700

    Change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getDefaultSettings } = require("http2");
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.get("/", (req, res) => {
   res.json({
