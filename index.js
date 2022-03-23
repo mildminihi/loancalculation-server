fb2e628a373f00dfbd6ca7c776290cb46beb8556
Supanat Wanroj <supanat.wanroj@scbtechx.io>
2022-03-23 17:23:19 +0700

    update medium loan

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getDefaultSettings } = require("http2");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 const port = 8000;
 
@@ -15,8 +15,8 @@ app.get("/flashLoan", (req, res) => {
   res.json(flashLoan(sampleData));
 });
 
-app.get("/medium", (req, res) => {
-
+app.get("/mediumLoan", (req, res) => {
+  res.json(mediumLoan(sampleData));
 });
 
 function flashLoan(userData) {
@@ -26,41 +26,43 @@ function flashLoan(userData) {
   let bondRatio = (bond * 90) / 100;
   let payPerMonthTotal = getOtherLoanPayPerMonth(userData.contracts, "ฉ");
   var incomePerMonth = salary - payPerMonthTotal;
-  var flashLoanTotal = 0;
+  var totalFlashLoan = 0;
   // ถ้าเป็นสมาชิกไม่ถึง 6 เดือนไม่มีสิทธิ์กู้ กู้ได้สูงสุดเท่ากับ2เท่าของเงินเดือนและไม่เกิน90%ของหุ้น
   if (isNewMember) {
-    flashLoanTotal = 0;
+    totalFlashLoan = 0;
   } else {
     if (incomePerMonth <= 2000) {
-      flashLoanTotal = 0;
+      totalFlashLoan = 0;
     } else {
-      if ((incomePerMonth * 2) >= bondRatio) {
-        flashLoanTotal = bondRatio;
+      if (incomePerMonth * 2 >= bondRatio) {
+        totalFlashLoan = bondRatio;
       } else {
-        flashLoanTotal = (Math.floor((incomePerMonth * 2) / 100) * 100);
+        totalFlashLoan = Math.floor((incomePerMonth * 2) / 100) * 100;
       }
     }
   }
   let previousFlashLoan = getOutStandingByType("ฉ", userData.contracts);
-  flashLoanTotal -= previousFlashLoan;
-  var flashLoanPerMonth = Math.ceil((flashLoanTotal / 12) / 10) * 10;
+  totalFlashLoan -= previousFlashLoan;
+  var flashLoanPerMonth = Math.ceil(totalFlashLoan / 12 / 10) * 10;
   var salaryAfterNewLoan = incomePerMonth - flashLoanPerMonth;
-  var flashLoanLastMonth = flashLoanTotal - (flashLoanPerMonth * 11);
+  var flashLoanLastMonth = totalFlashLoan - flashLoanPerMonth * 11;
 
   return {
-    flashLoanTotal: flashLoanTotal, //วงเงินกู้ทั้งหมด
+    totalFlashLoan: totalFlashLoan, //วงเงินกู้ทั้งหมด
     salaryBeforeNewLoan: incomePerMonth, //เงินได้รายเดือนก่อนกู้
     previousFlashLoan: previousFlashLoan, //ยอดเงินกู้ฉุกเฉินสัญญาเก่า
     newFlashLoanPerMonth: flashLoanPerMonth, //ยอดเงินกู้ต่อเดือน 1-11
     flashLoanLastMonth: flashLoanLastMonth, //ยอดเงินกู้เดือนสุดท้าย
-    salaryAfterNewLoan: salaryAfterNewLoan //เงินได้รายเดือนหลังกู้ฉุกเฉินใหม่
-  }
+    salaryAfterNewLoan: salaryAfterNewLoan, //เงินได้รายเดือนหลังกู้ฉุกเฉินใหม่
+  };
 }
 
 function getOutStandingByType(type, contracts) {
-  const result = contracts.filter(contract => contract.id.slice(0, 1) == type);
+  const result = contracts.filter(
+    (contract) => contract.id.slice(0, 1) == type
+  );
   if (result.length == 0) {
-    return 0
+    return 0;
   } else {
     return result[0].outStanding;
   }
@@ -68,7 +70,7 @@ function getOutStandingByType(type, contracts) {
 
 function getOtherLoanPayPerMonth(contracts, loanType) {
   var totalPayPerMonth = 0;
-  contracts.forEach(contract => {
+  contracts.forEach((contract) => {
     if (contract.id.slice(0, 1) != loanType) {
       let principle = contract.payPerMonth;
       let interest = getInterest(contract.id.slice(0, 1), contract.outStanding);
@@ -82,7 +84,7 @@ function getOtherLoanPayPerMonth(contracts, loanType) {
 function getInterest(type, outStanding) {
   var interest = 0;
   switch (type) {
-    case "ก", "ส", "ฉ":
+    case ("ก", "ส", "ฉ"):
       interest = 6.15;
       break;
     case "c":
@@ -95,7 +97,7 @@ function getInterest(type, outStanding) {
       interest = 5.9;
       break;
   }
-  return ((outStanding * interest * 30) / 36500);
+  return (outStanding * interest * 30) / 36500;
 }
 
 function getMemberStatus(memberStartDate) {
@@ -111,14 +113,47 @@ function getMemberStatus(memberStartDate) {
   }
 }
 
-function mediumLoan(memberPeriod) {
-  if (memberPeriod == 0.5) {
-    return 100000;
-  } else if (memberPeriod == 1) {
-    return 400000;
-  } else {
-    return 600000;
+function mediumLoan(userData) {
+  var totalMediumLoan;
+  let payPerMonthTotal = getOtherLoanPayPerMonth(userData.contracts, "ก");
+  var incomePerMonth = userData.salary - payPerMonthTotal;
+  var memberPeriod = getMemberStatus(userData.memberStartDate);
+  let age = parseInt(userData.age.slice(0,2));
+  // เชคเงื่อนไข อายุกการเป็นสมาชิก อายุสมาชิก เงินได้ต่อเดือน
+  if (memberPeriod == 0 || age >= 70 || incomePerMonth <= 2000) {
+    return {
+      totalMediumLoan: 0,
+    };
   }
+  if (memberPeriod == 0.5) {
+    totalMediumLoan = 100000;
+  } else if (memberPeriod == 1) {
+    totalMediumLoan = 400000;
+  } else {
+    totalMediumLoan = 600000;
+  }
+  var previousMediumLoan = getOutStandingByType("ก", userData.contracts);
+  totalMediumLoan -= previousMediumLoan;
+  var loanAbleYear = 70 - age;
+  var loanPeriod = 96;
+  var mediumLoanPerMonth;
+  // คำนวนงวด
+  if (loanAbleYear >= 8) {
+    mediumLoanPerMonth = Math.ceil(totalMediumLoan / loanPeriod / 10) * 10;
+  } else {
+    // คำนวนงวดถ้าอายุมากว่า 62 ปี
+  }
+  let mediumLoanLastMonth =
+    totalMediumLoan - mediumLoanPerMonth * (loanPeriod - 1);
+  let salaryAfterNewLoan = incomePerMonth - mediumLoanPerMonth;
+  return {
+    totalMediumLoan: totalMediumLoan, //วงเงินกู้ทั้งหมด
+    salaryBeforeNewLoan: incomePerMonth, //เงินได้รายเดือนก่อนกู้
+    previousMediumLoan: previousMediumLoan, //ยอดเงินกู้ปานกลางสัญญาเก่า
+    newMediumLoanPerMonth: mediumLoanPerMonth, //ยอดเงินกู้ต่องวด 1-ก่อนงวดสุดท้าย
+    mediumLoanLastMonth: mediumLoanLastMonth, //ยอดเงินกู้งวดสุดท้าย
+    salaryAfterNewLoan: salaryAfterNewLoan, //เงินได้รายเดือนหลังกู้ปานกลางใหม่
+  };
 }
 
 function getRetireSalary(retireYearTotal, salary) {
@@ -171,7 +206,7 @@ let sampleData = {
       approvalLimit: 429000,
       payPerMonth: 1800,
       paidPeriodTotal: 36,
-      outStanding: 364200
+      outStanding: 364200,
     },
     {
       id: "ก016770/2563",
@@ -180,10 +215,10 @@ let sampleData = {
       approvalLimit: 49000,
       payPerMonth: 600,
       paidPeriodTotal: 17,
-      outStanding: 38800
-    }
-  ]
-}
+      outStanding: 38800,
+    },
+  ],
+};
 
 // (364200 * 6.15 * 30) / 36500
 // ยอดคงเหลือ  ดอกเบี้ย
